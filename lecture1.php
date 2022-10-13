df4b601df6a9fa8d41af85c06a2cccb31e64728b
Harsh Kumar <9u.harsh@gmail.com>
2022-10-13 10:57:03 +0530

    php files added

---
+++ lecture1.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello world";
+echo " My name is Harsh";
+echo " I love coding";
+
+?>
---
+++ lecture3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Variables in php</h1>
+</body>
+</html>
+
+<?php
+
+echo "variable are declared in php with first letter as '$' sign and then letters or underscore and contains no number  like";
+
+?>
