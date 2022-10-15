83117da8dc2dc76416edd6046a826611aa5ed3ae
Harsh Kumar <9u.harsh@gmail.com>
2022-10-15 09:56:27 +0530

    more files added

---
+++ lecture8.php
@@ -0,0 +1,7 @@
+<?php
+echo "this is harsh";
+$r = 2;
+$pi = 3.14;
+$a = $pi*pow($r,2);
+echo "<br/>Area is $a";
+?>
---
+++ lecture9.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = 12;
+echo ++$a;
+echo "<br />" .$a++;
+echo "<br />" .$a;
+
+?>
